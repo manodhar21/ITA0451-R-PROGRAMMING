e7141decc21065b58bb435f026781941ad4f4d1a
manodhar21 <114211383+manodhar21@users.noreply.github.com>
2023-06-20 11:35:23 +0530

    Create List of objects.r

---
+++ List of objects.r
@@ -0,0 +1,15 @@
+# create the list object
+
+store_items <- list(
+
+  Fruits = c("orange", "mango", "apple", "watermelon", "banana"),
+
+  Juices = c("appy", "fruity", "slice"),
+
+  Milkshakes = c("Mango", "papaya", "chocolate", "Rose")
+
+)
+
+# display the list object
+
+print(store_items)
