de49e291c797a1263ca4c72b8fef6c9307488908
manodhar21 <114211383+manodhar21@users.noreply.github.com>
2023-06-20 10:51:44 +0530

    Create Assign.r

---
+++ Assign.r
@@ -0,0 +1,39 @@
+years_of_experience <- c(8, 10, 10, 1, 10, 10, 8, 12, 1, 12)
+
+# (i) sum of the elements in years_of_experience
+
+sum_years_of_experience <- sum(years_of_experience)
+
+print(sum_years_of_experience)
+
+# (ii) average of the elements in years_of_experience
+
+avg_years_of_experience <- mean(years_of_experience)
+
+print(avg_years_of_experience)
+
+# (iii) median value of the elements in years_of_experience
+
+median_years_of_experience <- median(years_of_experience)
+
+print(median_years_of_experience)
+
+# (iv) length of the years_of_experience variable
+
+length_years_of_experience <- length(years_of_experience)
+
+print(length_years_of_experience)
+
+# (v) standard deviation of the values in years_of_experience
+
+sd_years_of_experience <- sd(years_of_experience)
+
+print(sd_years_of_experience)
+
+# (vi) standard deviation of the elements in years_of_experience, then round it
+
+round_sd_years_of_experience <- round(sd(years_of_experience))
+
+print(round_sd_years_of_experience)
+
+# (vii) standard deviation of the elements in
